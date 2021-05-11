affdd86a00cf114e6f23cdad9c35641a34540c3f
VikhilP <vikhilp98@gmail.com>
2021-05-11 15:03:38 +0100

    relationships finsihed

--- flaskrelationships/app.py
+++ flaskrelationships/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import url_for
 from flask_sqlalchemy import SQLAlchemy
+import create.py
 
 app = Flask(__name__)
 
@@ -10,11 +11,16 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 class GameSeries(db.Model):
     series_id = db.Column(db.Integer, primary_key=True)
-    series_name = db.Column(db.String(50), nullable=False)
+    series_name = db.Column(db.String(50), nullable=False, unique=True)
     review = db.Column(db.String(50))
     games = db.relationship("Game", backref="seriesname")
 
 class Game(db.Model):
     game_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
-    series = db.Column(db.Integer, db.ForeignKey("game_series.series_id"))
+    #series = db.Column(db.Integer, db.ForeignKey("game_series.series_id"))
+    series = db.Column(db.String(50), db.ForeignKey("game_series.series_name"))
+
+
+
+
--- flaskrelationships/create.py
+++ flaskrelationships/create.py
@@ -3,11 +3,17 @@ from app import db, GameSeries, Game
 db.drop_all()
 db.create_all()
 gameseries_yakuza = GameSeries(series_name = "Yakuza", review = "9/10")
-game_yakuza0 = Game(name = "Yakuza 0", series = 1)
-game_yakuza1 = Game(name = "Yakuza Kiwami", series = 1)
-
-
 db.session.add(gameseries_yakuza)
+db.session.commit() #commit here so i can use GameSeries.query.filter_by(series_name='Yakuza').first().series_name other wise it won't search through it
+
+
+game_yakuza0 = Game(name = "Yakuza 0", seriesname = gameseries_yakuza)
+game_yakuza1 = Game(name = "Yakuza Kiwami", series = GameSeries.query.filter_by(series_name='Yakuza').first().series_name)
+
+# print(game_yakuza1.seriesname.review)
+
+
+# db.session.add(gameseries_yakuza)
 
 db.session.add(game_yakuza0)
 db.session.add(game_yakuza1)
