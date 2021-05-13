14e7c8a84fb2371c6b5d7b329d5e15d77e6d1db1
VikhilP <vikhilp98@gmail.com>
2021-05-13 16:03:10 +0100

    4pm progress

--- todo2/appfolder/models.py
+++ todo2/appfolder/models.py
@@ -1,6 +1,7 @@
+from wtforms.fields.core import SelectField
 from appfolder import db
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField,SelectField
 
 
 class Todos(db.Model):
@@ -12,3 +13,7 @@ class TodoForm(FlaskForm):
     task = StringField('Task')
     submit = SubmitField('Add Todo')
 
+class IndexForm(FlaskForm):
+    changestate = SelectField("Change Status", choices=[("complete","complete"), ("incomplete", "incomplete")])
+    delete = SubmitField("Delete Task")
+    save = SubmitField("Save Changes")
--- todo2/appfolder/routes.py
+++ todo2/appfolder/routes.py
@@ -1,5 +1,5 @@
 from appfolder import app, db
-from appfolder.models import Todos, TodoForm
+from appfolder.models import Todos, TodoForm, IndexForm
 from flask import render_template, request, redirect, url_for
 
 @app.route('/add/<todo>')
@@ -18,7 +18,31 @@ def complete(todoid):
         db.session.commit()
         return f"{temp.task} is complete"
     return "error"
+
+@app.route('/complete', methods = ["POST"])
+def complete2():
+    temp = request.form.get("id")
+    temptask = Todos.query.filter_by(id=temp).first()
+    temptask.done = True
+    db.session.commit()
+    return redirect("/")
+
+@app.route('/incomplete', methods = ["POST"])
+def incomplete():
+    temp = request.form.get("id")
+    temptask = Todos.query.filter_by(id=temp).first()
+    temptask.done = False
+    db.session.commit()
+    return redirect(url_for("index"))
     
+
+@app.route('/delete', methods=["POST"])
+def delete2():
+    temp = request.form.get("id")
+    temptask = Todos.query.filter_by(id=temp).first()
+    db.session.delete(temptask)
+    db.session.commit()
+    return redirect(url_for("index"))
 
 @app.route('/delete/<todoid>')
 def delete(todoid):
@@ -28,9 +52,31 @@ def delete(todoid):
     db.session.commit()
     return f"{temptask} has been deleted"
 
-@app.route('/')
+@app.route('/', methods = ["GET", "POST"])
 def index():
-    return render_template("index.html", all_todos = Todos.query.all() )
+    form = IndexForm()
+    return render_template("index.html", form=form, all_todos = Todos.query.all() )
+
+
+@app.route("/update", methods = ["GET", "POST"])
+def update():
+    error = ""
+    temp = request.form.get("id")
+    temptask = Todos.query.filter_by(id=temp).first()
+    form = TodoForm()
+
+
+    if request.method == "POST":
+        _task = form.task.data
+
+        if len(_task) ==0:
+            error = "You cannot update data to be Null"
+        else:
+            temptask.task = _task
+            db.session.commit()
+            return redirect(url_for("index"))
+        
+    return render_template('update.html', form=form, message = error, todo = temptask)
 
 @app.route("/addform", methods = ["GET", "POST"])
 def addform():
